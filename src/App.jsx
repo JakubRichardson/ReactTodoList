9463e225b0eb081cf18156a4bbd9e5a28f8ea4a9
Jakub Richardson <pythonbottest2020@gmail.com>
2023-06-09 18:38:03 +0200

    Add dark mode, including media query

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,30 @@ import CssBaseline from "@mui/material/CssBaseline";
 import './App.css';
 import TodoList from './TodoList';
 
+import { useMemo } from "react";
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <h1>Todos</h1>
       <TodoList />
-    </>
+    </ThemeProvider>
   );
 }
 
